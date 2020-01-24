2b1f258b0dc1a1fbc43aedc9a21d7f589a524277
Maelle <maelle.banor974@laposte.net>
2020-01-24 14:16:02 +0100

    Ajout fonction infosBoissons()

--- distri_boisson_main.py
+++ distri_boisson_main.py
@@ -2,3 +2,33 @@ from distributeur.distributeur_class import Distributeur, Boisson
 
 
 
+superPorp = Boisson("Super Porp", "Soda")
+durianJuice = Boisson("Durian Juice", "Juice")
+duff = Boisson("Duff", "Bière")
+slurm = Boisson("Slurm", "Soda")
+
+
+
+distri = Distributeur(superPorp, durianJuice, duff, slurm)
+
+
+
+
+print("Bonjour ! Bienvenue au Distri'Boisson !")
+reponseUti = input(" Appuyez sur \'B\' pour voir la liste des boissons proposées. \n Appuyez sur \'X\' pour partir.")
+
+#tant que l'utilisateur ne choisit pas l'une des options proposées ... 
+while reponseUti.upper() != 'X' and reponseUti.upper() != 'B':
+	reponseUti = input("Pardon, je n'ai pas compris ce que vous aviez écrit. Merci de recommencer.")
+
+if reponseUti.upper() == 'X':
+	print("Merci de votre visite, à bientôt !")
+
+elif reponseUti.upper() == 'B':
+	print(distri)
+	reponseUti = input("Appuyez sur le numéro correspondant à votre boisson pour la commander. \nSinon, entrez \'Infos\' suivi du numéro de votre boisson pour avoir des informations.")
+	if isinstance(reponseUti, str):
+		if distri.infosBoissons(reponseUti) == False:
+			print("Cette boisson n'existe pas, veuillez recommencer !")
+		else:
+			print(distri.infosBoissons(reponseUti))
--- distributeur/distributeur_class.py
+++ distributeur/distributeur_class.py
@@ -9,7 +9,7 @@ class Boisson:
 		self.quantite = quantite
 
 	def __str__(self):
-		return "Nom : {} \nType : {} \nPrix : {} \nQuantité restante : {}".format(self.nom, self.typeB, self.prix, self.quantite) 
+		return "Nom : {} \nType : {} \nPrix : {}€ \nQuantité restante : {}".format(self.nom, self.typeB, self.prix, self.quantite) 
 
 	#Méthode pour remettre des boissons en stock
 	def remettreEnStock(nbBoissonAjoute):
@@ -24,8 +24,29 @@ class Distributeur:
 	def __init__(self, *boissons):
 		self.boissons = boissons
 
-	#def proposerBoissons():
+	def __str__(self):
+		phrase = "Voici les boissons disponibles : \n\n"
+		for i in range(0,len(self.boissons) - 1):
+			phrase+="Boisson n°{} : {} \n".format(i+1,self.boissons[i].nom)
+		return phrase
+
+	#Lorsque l'utilisateur souhaite avoir des informations sur une boisson
+	def infosBoissons(self, reponseUti):
+		infos = "infos"
+
+		#on transforme la réponse de l'utilisateur en enlevant les potentiels espaces et majuscules
+		newReponseUti = reponseUti.lower().replace(" ", "")
 		
-		
+
+		for i in range(0,len(self.boissons) - 1):
+			infos+=str(i+1)
+			#si le numéro choisi par l'utilisateur correspond bien à une boisson, on renvoie les informations demandées
+			if infos == newReponseUti:
+				return self.boissons[i]
+
+			#on redonne à la variable infos sa valeur d'origine, afin de pouvoir continuer à la tester	
+			infos = "infos"
+
+		return False
 	
 
