216ba34a65e1f0f9d5367ff46cb62c2c10889cf5
Maelle <maelle.banor974@laposte.net>
2020-01-27 11:02:23 +0100

    Retour en arrière, affichage des boissons

--- distri_boisson_main.py
+++ distri_boisson_main.py
@@ -3,32 +3,71 @@ from distributeur.distributeur_class import Distributeur, Boisson
 
 
 superPorp = Boisson("Super Porp", "Soda")
-durianJuice = Boisson("Durian Juice", "Juice")
+durianJuice = Boisson("Durian Juice", "Jus de fruit")
 duff = Boisson("Duff", "Bière")
 slurm = Boisson("Slurm", "Soda")
 
 
-
+#Dictionnaire d'objets qui ont comme "attribut" une quantité
+#distri = Distributeur(superPorp, durianJuice, duff, slurm)
 distri = Distributeur(superPorp, durianJuice, duff, slurm)
+print(distri)
 
 
 
 
-print("Bonjour ! Bienvenue au Distri'Boisson !")
-reponseUti = input(" Appuyez sur \'B\' pour voir la liste des boissons proposées. \n Appuyez sur \'X\' pour partir.")
+# print("Bonjour ! Bienvenue au Distri'Boisson !")
+# reponseUti = input(" Appuyez sur \'B\' pour voir la liste des boissons proposées. \n Appuyez sur \'X\' pour partir.")
+# module = ""
+
+# while module != "Exit":
+
+# 	if reponseUti.upper() == 'B':
+# 		module = "afficherBoissons"
+
+# 	if module == "afficherBoissons":
+# 		print(distri)
+
+# 	if "infos" in reponseUti.lower():
+# 		module = "afficherInformations"
+
+# 	if module == "afficherInformations":
+# 		if distri.infosBoissons(reponseUti) == False:
+#  			print("Cette boisson n'existe pas!")
+#  			module = "afficherBoissons"
+#  		else:
+#  			print(distri.infosBoissons(reponseUti))
+#  			reponseUti = input("Appuyez sur \'B\' pour revenir au choix des boissons")
+
+#  	if isinstance(reponseUti, int) and module != "paiement":
+#  		module = "Commande"
+
+#  	if module == "Commande":
+#  		verif = distri.verifCommande(reponseUti)
+#  		if verif == False:
+#  			print("Cette boisson n'existe pas !")
+#  		elif verif == True:
+#  			module = "Paiement"
+#  		else:
+#  			print distri.verifCommande(reponseUti)
+
+
+		
 
 #tant que l'utilisateur ne choisit pas l'une des options proposées ... 
-while reponseUti.upper() != 'X' and reponseUti.upper() != 'B':
-	reponseUti = input("Pardon, je n'ai pas compris ce que vous aviez écrit. Merci de recommencer.")
+# while reponseUti.upper() != 'X' and reponseUti.upper() != 'B':
+# 	reponseUti = input("Pardon, je n'ai pas compris ce que vous aviez écrit. Merci de recommencer.")
 
-if reponseUti.upper() == 'X':
-	print("Merci de votre visite, à bientôt !")
+# if reponseUti.upper() == 'X':
+# 	print("Merci de votre visite, à bientôt !")
 
-elif reponseUti.upper() == 'B':
-	print(distri)
-	reponseUti = input("Appuyez sur le numéro correspondant à votre boisson pour la commander. \nSinon, entrez \'Infos\' suivi du numéro de votre boisson pour avoir des informations.")
-	if isinstance(reponseUti, str):
-		if distri.infosBoissons(reponseUti) == False:
-			print("Cette boisson n'existe pas, veuillez recommencer !")
-		else:
-			print(distri.infosBoissons(reponseUti))
+# elif reponseUti.upper() == 'B':
+# 	print(distri)
+# 	reponseUti = input("Appuyez sur le numéro correspondant à votre boisson pour la commander. \nSinon, entrez \'Infos\' suivi du numéro de votre boisson pour avoir des informations.")
+# 	if isinstance(reponseUti, str):
+# 		if distri.infosBoissons(reponseUti) == False:
+# 			print("Cette boisson n'existe pas, veuillez recommencer !")
+# 		else:
+# 			print(distri.infosBoissons(reponseUti))
+# 			
+
--- distributeur/distributeur_class.py
+++ distributeur/distributeur_class.py
@@ -2,18 +2,13 @@ class Boisson:
 	"""docstring for Boisson"""
 
 	#Constructeur paramétré	
-	def __init__(self, nom="Coca", typeB="Soda", prix=2, quantite=0):
+	def __init__(self, nom="Coca", typeB="Soda", prix=2):
 		self.nom = nom
 		self.typeB = typeB
 		self.prix = prix
-		self.quantite = quantite
-
+		
 	def __str__(self):
-		return "Nom : {} \nType : {} \nPrix : {}€ \nQuantité restante : {}".format(self.nom, self.typeB, self.prix, self.quantite) 
-
-	#Méthode pour remettre des boissons en stock
-	def remettreEnStock(nbBoissonAjoute):
-		self.quantite+=nbBoissonAjoute
+		return "Nom : {} \nType : {} \nPrix : {}€".format(self.nom, self.typeB, self.prix) 
 
 	
 class Distributeur:
@@ -21,32 +16,57 @@ class Distributeur:
 	"""docstring for Distributeur"""
 	
 	#Constructeur paramétré
-	def __init__(self, *boissons):
-		self.boissons = boissons
-
-	def __str__(self):
-		phrase = "Voici les boissons disponibles : \n\n"
-		for i in range(0,len(self.boissons) - 1):
-			phrase+="Boisson n°{} : {} \n".format(i+1,self.boissons[i].nom)
-		return phrase
-
-	#Lorsque l'utilisateur souhaite avoir des informations sur une boisson
-	def infosBoissons(self, reponseUti):
-		infos = "infos"
-
-		#on transforme la réponse de l'utilisateur en enlevant les potentiels espaces et majuscules
-		newReponseUti = reponseUti.lower().replace(" ", "")
+	def __init__(self, *args):
+		self.boissonsD = []
+		for arg in args:
+			self.boissonsD.append(arg)
 		
 
-		for i in range(0,len(self.boissons) - 1):
-			infos+=str(i+1)
-			#si le numéro choisi par l'utilisateur correspond bien à une boisson, on renvoie les informations demandées
-			if infos == newReponseUti:
-				return self.boissons[i]
+	def __str__(self):
+		phrase = ""
+		i = 1
+		for boisson in self.boissonsD:
+			phrase+="Boisson n°{} : {} \n".format(i,boisson)
+			i = i+1
+		return phrase
 
-			#on redonne à la variable infos sa valeur d'origine, afin de pouvoir continuer à la tester	
-			infos = "infos"
 
-		return False
+
+		# phrase = "Voici les boissons disponibles : \n\n"
+		# for i in range(0,len(self.boissons) - 1):
+		# 	phrase+="Boisson n°{} : {} \n".format(i+1,self.boissons[i].nom)
+		# return phrase
+
+	# #Lorsque l'utilisateur souhaite avoir des informations sur une boisson
+	# def infosBoissons(self, reponseUti):
+	# 	infos = "infos"
+
+	# 	#on transforme la réponse de l'utilisateur en enlevant les potentiels espaces et majuscules
+	# 	newReponseUti = reponseUti.lower().replace(" ", "")
+		
+
+	# 	for i in range(0,len(self.boissons) - 1):
+	# 		infos+=str(i+1)
+	# 		#si le numéro choisi par l'utilisateur correspond bien à une boisson, on renvoie les informations demandées
+	# 		if infos == newReponseUti:
+	# 			return self.boissons[i]
+
+	# 		#on redonne à la variable infos sa valeur d'origine, afin de pouvoir continuer à la tester	
+	# 		infos = "infos"
+
+	# 	return False
 	
+	# def verifCommande(self, reponseUti):
+
+	# 	for i in range(0,len(self.boissons) - 1):
+	# 		#je vérifie si la boisson commandée se trouve dans mon tableau
+	# 		if reponseUti == i + 1:
+	# 			#je vérifie si la boisson est toujours en stock
+	# 			if self.boissons[i].quantite > 0:
+	# 			return True
+	# 			else:
+	# 			return "Cette boisson n'est plus en stock."	
+		
+	# 	return False
+
 
