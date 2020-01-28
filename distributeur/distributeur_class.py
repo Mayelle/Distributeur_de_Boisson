ab1bc302ec921d30d4c7e3b41074481c8b6567cf
Maelle <maelle.banor974@laposte.net>
2020-01-28 17:49:10 +0100

    Ajout gestion rendu monnaie

--- distributeur/distributeur_class.py
+++ distributeur/distributeur_class.py
@@ -26,9 +26,15 @@ class Distributeur:
 	"rupture_stock": "Cette boisson n'est malheureusement plus en stock ! Veuillez en choisir une autre.",
 	"confirmation_choix": "Êtes-vous sûr de vouloir commander la boisson {} (oui/ non)? ",
 	"degustation": "Voici votre boisson. Bonne dégustation !",
-	"nouvelle_commande": "Voulez-vous commander une autre boisson ? (oui / non)"
+	"nouvelle_commande": "Voulez-vous commander une autre boisson ? (oui / non)",
+	"demande_paiement": "Veuillez entrez le montant de la pièce que vous souhaitez insérer dans la machine : ",
+	"montant_du": "Vous devez payer encore {} €",
+	"montant_non_accepte" : "Ce montant n'est pas accepté par la machine. Merci de recommencer",
+	"montant_rendu": "La machine vous rend la monnaie : vous avez récupéré "
 
 	}
+
+	PIECES_ACCEPTEES = [2.0,1.0,0.5,0.2,0.1,0.05,0.02,0.01]
 
 
 
@@ -79,14 +85,13 @@ class Distributeur:
 			
 
 	def __verif_choix_utilisateur(self):
-		reponse_utilisateur = input(self.REPONSES["commande_boisson"])
 		try:
-			new_reponse_utilisateur = int(reponse_utilisateur.replace(" ", ""))
+			new_reponse_utilisateur = int(input(self.REPONSES["commande_boisson"]).replace(" ", ""))
 
 			try:
 				if new_reponse_utilisateur > 0:
-					nom_boisson_commandee = self.boissons[new_reponse_utilisateur - 1].nom
-					self.__verif_quantite_boisson_restante(nom_boisson_commandee)
+					boisson_commandee = self.boissons[new_reponse_utilisateur - 1]
+					self.__verif_quantite_boisson_restante(boisson_commandee)
 				else:
 					print(self.REPONSES["non_existante"])
 					self.__verif_choix_utilisateur()
@@ -102,29 +107,101 @@ class Distributeur:
 
 
 
-	def __verif_quantite_boisson_restante(self, nom_boisson_commandee):
-		if self.quantite[nom_boisson_commandee] > 0:
-			self.__confirmation_utilisateur(nom_boisson_commandee)
+	def __verif_quantite_boisson_restante(self, boisson_commandee):
+		if self.quantite[boisson_commandee.nom] > 0:
+			self.__confirmation_utilisateur(boisson_commandee)
 		else:
 			print(self.REPONSES["rupture_stock"])
 			return self.__verif_choix_utilisateur()
 
 	
-	def __confirmation_utilisateur(self, nom_boisson_commandee):
-		reponse_utilisateur = input(self.REPONSES["confirmation_choix"].format(nom_boisson_commandee))
+	def __confirmation_utilisateur(self, boisson_commandee):
+		reponse_utilisateur = input(self.REPONSES["confirmation_choix"].format(boisson_commandee.nom))
 		while reponse_utilisateur != "oui" and reponse_utilisateur != "non":
 			print(self.REPONSES["mauvaise_entree"])
-			self.__confirmation_utilisateur(nom_boisson_commandee)
+			self.__confirmation_utilisateur(boisson_commandee)
 		if reponse_utilisateur.lower().replace(" ", "") == "oui":
-			self.__commande_boisson(nom_boisson_commandee)
+			return self.__demande_paiement(boisson_commandee)
 		else:
 			self.__verif_choix_utilisateur()
 
+
+
+	def __demande_paiement(self, boisson_commandee, montant_paye = 0):
+		montant_du = boisson_commandee.prix - montant_paye
+
+		try:
+			reponse_utilisateur = float(input(self.REPONSES["demande_paiement"]).replace(" ", ""))
+
+			if self.__verif_piece_inseree(reponse_utilisateur) == True:
+				
+
+				montant_paye += reponse_utilisateur
+				montant_du -= reponse_utilisateur
+				
+			
+				while montant_du > 0:
+					print(self.REPONSES["montant_du"].format(round(montant_du,2)))
+					return self.__demande_paiement(boisson_commandee, montant_paye)
+
+				return self.__rendu_monnaie(boisson_commandee, montant_du)
+
+			else:
+				print(self.REPONSES["montant_non_accepte"])
+				return self.__demande_paiement(boisson_commandee, montant_paye)
+
+		except:
+			print(self.REPONSES["mauvaise_entree"])
+			return self.__demande_paiement(boisson_commandee, montant_paye)
+
+
+	def __verif_piece_inseree(self, piece):
+		for i in range(0,len(self.PIECES_ACCEPTEES)):
+			if piece == self.PIECES_ACCEPTEES[i]:
+				return True
 		
+		return False
+
+	def __rendu_monnaie(self, boisson_commandee, montant_du):
+		if montant_du == 0:
+			return self.__commande_boisson(boisson_commandee)
+		else:
+			monnaie_rendue = []
+			montant_rendu = abs(montant_du)
+			detail_monnaie_rendue = self.REPONSES["montant_rendu"]
+
+			step = 0
+			while round(montant_rendu,2) != 0.00:
+				for i, x in enumerate(self.PIECES_ACCEPTEES[step:]):
+					
+					if round(montant_rendu,2) - x < 0:
+						continue
+						
+					if round(montant_rendu,2) - x > 0:
+						monnaie_rendue.append(x)
+						montant_rendu = round(montant_rendu,2) - x
+						step = i
+						break
+
+					if round(montant_rendu,2) - x == 0:
+						monnaie_rendue.append(x)
+						montant_rendu = round(montant_rendu,2) - x
+						
+						break
+
+
+			for i in self.PIECES_ACCEPTEES:
+				if monnaie_rendue.count(i) > 0:
+					detail_monnaie_rendue+="{} pièce(s) de {} €, ".format(monnaie_rendue.count(i),i)
+				
+			print(detail_monnaie_rendue)
+			return self.__commande_boisson(boisson_commandee)
+
+								
 	
-	def __commande_boisson(self, nom_boisson_commandee):
+	def __commande_boisson(self, boisson_commandee):
 		print(self.REPONSES["degustation"])
-		self.quantite[nom_boisson_commandee] -= 1
+		self.quantite[boisson_commandee.nom] -= 1
 		recommencer = input(self.REPONSES["nouvelle_commande"]).lower().replace(" ", "")
 		while recommencer != "oui" and recommencer != "non":
 			recommencer = input(self.REPONSES["mauvaise_entree"])
@@ -132,49 +209,4 @@ class Distributeur:
 			self.__affichage_boissons(recommencer)
 		else:
 			print(self.REPONSES["au_revoir"])
-
-
-
-
-
-
-
-
-		# phrase = "Voici les boissons disponibles : \n\n"
-		# for i in range(0,len(self.boissons) - 1):
-		# 	phrase+="Boisson n°{} : {} \n".format(i+1,self.boissons[i].nom)
-		# return phrase
-
-	# #Lorsque l'utilisateur souhaite avoir des informations sur une boisson
-	# def infosBoissons(self, reponse_utilisateur):
-	# 	infos = "infos"
-
-	# 	#on transforme la réponse de l'utilisateur en enlevant les potentiels espaces et majuscules
-	# 	new_reponse_utilisateur = reponse_utilisateur.lower().replace(" ", "")
-		
-
-	# 	for i in range(0,len(self.boissons) - 1):
-	# 		infos+=str(i+1)
-	# 		#si le numéro choisi par l'utilisateur correspond bien à une boisson, on renvoie les informations demandées
-	# 		if infos == new_reponse_utilisateur:
-	# 			return self.boissons[i]
-
-	# 		#on redonne à la variable infos sa valeur d'origine, afin de pouvoir continuer à la tester	
-	# 		infos = "infos"
-
-	# 	return False
-	
-	# def verifCommande(self, reponse_utilisateur):
-
-	# 	for i in range(0,len(self.boissons) - 1):
-	# 		#je vérifie si la boisson commandée se trouve dans mon tableau
-	# 		if reponse_utilisateur == i + 1:
-	# 			#je vérifie si la boisson est toujours en stock
-	# 			if self.boissons[i].quantite > 0:
-	# 			return True
-	# 			else:
-	# 			return "Cette boisson n'est plus en stock."	
-		
-	# 	return False
-
 
